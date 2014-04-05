f760297cfebbe34d55c2f917b9ea4c52c354e901
welchbm <brandon.welch@utah.edu>
2014-04-05 09:43:12 -0600

    working on wget

--- check-clinvar/check.py
+++ check-clinvar/check.py
@@ -18,19 +18,27 @@ url="ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/xml/"
 soup= BeautifulSoup(urllib2.urlopen(url))
 all_rel= soup.p.string
 search=re.compile(r"""[A-Z][a-z]{2,2}\s{1,2}[0-9]{1,2}\s{1,2}[0-9]{1,2}:[0-9]{1,2}""")
-latest_release = search.findall(all_rel)[0]
+latest_release = str(search.findall(all_rel)[0])
 
-print last_download
-print latest_release
+# print last_download
+# print latest_release
+
+#File system variables
+new_name=latest_release.translate(None,': ')
+file_loc='~/projects/VisualizeClinvar/check-clinvar/clinvar-downloads/'
+cv='ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/xml/ClinVarFullRelease_00-latest.xml.gz'
+
+# print new_name
 
 if last_download == latest_release:
     print 'Its a match'
     pass
 else:
     print "Downloading"
-    #subprocess.call("wget ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/xml/ClinVarFullRelease_00-latest.xml.gz")
-    #subprocess.call(gunzip the downloaded file)
+    os.system("""wget -P %s -O clinvar-downloads/%s.xml.gz %s""" %(file_loc,new_name,cv))
+    os.system("""gunzip %s%s.xml.gz""" %(file_loc,new_name))
     #subprocess.call(xsltproc downloaded file)
+
     file=open(filename, "a")
     file.write("\n"+latest_release)
     file.close()
