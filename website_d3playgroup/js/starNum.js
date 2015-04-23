967a06d0790afeea3cec2a51c9b82dba53feade3
bheale <bret.heale@hsc.utah.edu>
2015-04-22 22:17:37 -0600

    added dev folder for d3 imagineers
    website_d3playgroup

---
+++ website_d3playgroup/js/starNum.js
@@ -0,0 +1,134 @@
+//gives browser instruction to run on load and adds it as a procedure so that other scripts will also run
+if(window.onload) {
+        var curronload = window.onload;
+        var newonload = function() {
+            curronload();
+            starNum();
+        };
+        window.onload = newonload;
+} else {
+    window.onload = starNum();
+}
+//taken from http://stackoverflow.com/questions/641857/javascript-window-resize-event I apologize for complexity
+//
+var addEvent = function(elem, type, eventHandle) {
+    if (elem == null || typeof(elem) == 'undefined') return;
+    if ( elem.addEventListener ) {
+        elem.addEventListener( type, eventHandle, false );
+    } else if ( elem.attachEvent ) {
+        elem.attachEvent( "on" + type, eventHandle );
+    } else {
+        elem["on"+type]=eventHandle;
+    }
+};
+
+function starNum() {
+  var colwidth = parseInt(d3.select("#starNum").style("width"),0);
+  var margin = {top: parseInt(0.05 * colwidth, 0),
+                right: parseInt(0.4 * colwidth, 0),
+                bottom: parseInt(0.05 * colwidth, 0),
+                left: parseInt(0.1 * colwidth, 0)};
+  var width = parseInt(0.5 * colwidth, 0);
+  var height = parseInt(0.40 * colwidth, 0);
+
+var parseDate = d3.time.format("%y-%b-%d").parse,
+    formatPercent = d3.format(".0%");
+
+var x = d3.time.scale()
+    .range([0, width]);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var color = d3.scale.category20();
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .tickFormat(formatPercent);
+
+var area = d3.svg.area()
+    .x(function(d) { return x(d.date); })
+    .y0(function(d) { return y(d.y0); })
+    .y1(function(d) { return y(d.y0 + d.y); });
+
+var stack = d3.layout.stack()
+    .values(function(d) { return d.values; });
+
+
+var svg = d3.select("#starNum").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.tsv("data/starNum/starNum.tsv", function(error, data) {
+  color.domain(d3.keys(data[0]).filter(function(key) { return key !== "date"; }));
+
+  data.forEach(function(d) {
+    d.date = parseDate(d.date);
+  });
+
+  var browsers = stack(color.domain().map(function(name) {
+    return {
+      name: name,
+      values: data.map(function(d) {
+        return {date: d.date, y: d[name] / 100};
+      })
+    };
+  }));
+
+  x.domain(d3.extent(data, function(d) { return d.date; }));
+
+  var browser = svg.selectAll(".browser")
+      .data(browsers)
+    .enter().append("g")
+      .attr("class", "browser");
+
+  browser.append("path")
+      .attr("class", "area")
+      .attr("d", function(d) { return area(d.values); })
+      .style("fill", function(d) { return color(d.name); });
+
+  //browser.append("text")
+      //.datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
+      //.attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.y0 + d.value.y / 2) + ")"; })
+      //.attr("x", -200)
+      //.attr("dy", ".35em")
+      //.text(function(d) { return d.name; });
+
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis);
+
+	var legend = svg.selectAll(".legend")
+		.data(color.domain())
+		.enter().append("g")
+		.attr("class", "legend")
+		.attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+	legend.append("rect")
+      .attr("x", width + 5)
+      .attr("width", 14)
+      .attr("height", 14)
+      .style("fill", color);
+
+  leg_font_size = 
+  legend.append("text")
+      .attr("x", width + 28)
+      .attr("y", 9)
+      .attr("dy", ".35em")
+      .attr("font-size", ".7em")
+      .style("text-anchor", "start")
+      .text(function(d) { return d; });
+});
+}
