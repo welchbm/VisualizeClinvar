05925b0e3134ffef9744df789f32e1ba29e8d90e
Jerry Hardee <hardeeje@musc.edu>
2015-06-18 08:18:15 -0400

    Started rails app.

---
+++ VisualizeClinvar/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VisualizeClinvar::Application.config.secret_token = 'd983728e9e013d64651fd2a1be30a5624debdd56714f99dcd63eefd34e7b32ef950a92d1f4d0160f266b653bd8f0bda3c60f2ebd9ae4f3c4a6ddbf440643831d'
