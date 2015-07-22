dd65aedd8f13c6fde5b883e8459a59cab56a679b
Andrew Cates <amcates@gmail.com>
2015-07-22 14:18:04 -0400

    adding secrets.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :log_level, :debug
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', '.env', '.ruby-version', '.ruby-gemset')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', '.env', '.ruby-version', '.ruby-gemset')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
